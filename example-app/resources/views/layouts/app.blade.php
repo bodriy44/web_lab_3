aec726384c16e9d679e86c57e4d81f00594ed5f7
Бадрединов Эльдар <bodriy666@gmail.com>
2021-11-16 19:00:29 +0400

    Мелкие исправления

---
+++ example-app/resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+  <div id="app">
+    <main class="py-4">
+        @yield('content')
+    </main>
+  </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
