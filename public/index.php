7849632137ee030a95b1281f517b7219f816ba91
Бадрединов Эльдар <bodriy666@gmail.com>
2021-11-16 14:57:27 +0400

    добавлены php файлы

--- public/index.php
+++ public/index.php
@@ -1,3 +1,19 @@
 <?php
 
-echo phpinfo();
+require_once('LoginForm.php');
+
+// для изображений
+var_dump($_FILES);
+
+$form = new LoginForm($_POST);
+
+if (!$form->validate()) {
+    var_dump($form->getErrors());
+}
+
+if (!$form->login()) {
+    var_dump($form->getErrors());
+} else {
+    $authUser = $form->getAuthUser();
+    echo $authUser['login'] . ', Вы авторизовались!';
+}
