9f354aae5287e15d4ee79c9d051fe4e51809d402
Бадрединов Эльдар <bodriy666@gmail.com>
2021-12-04 14:49:03 +0300

    Перенес содержимое папки web-app в корень

--- public/index.php
+++ public/index.php
@@ -1,19 +1,55 @@
 <?php
 
-require_once('LoginForm.php');
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
-// для изображений
-var_dump($_FILES);
+define('LARAVEL_START', microtime(true));
 
-$form = new LoginForm($_POST);
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
 
-if (!$form->validate()) {
-    var_dump($form->getErrors());
+if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
+    require __DIR__.'/../storage/framework/maintenance.php';
 }
 
-if (!$form->login()) {
-    var_dump($form->getErrors());
-} else {
-    $authUser = $form->getAuthUser();
-    echo $authUser['login'] . ', Вы авторизовались!';
-}
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
